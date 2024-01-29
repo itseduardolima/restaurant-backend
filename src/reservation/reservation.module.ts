601231003e3248dfe2c890fee779dfc4714c3db9
itseduardolima <eduardolima2417@gmail.com>
2024-01-29 14:16:52 -0400

    fix: schedules

--- src/reservation/reservation.module.ts
+++ src/reservation/reservation.module.ts
@@ -3,9 +3,14 @@ import { ReservationService } from './reservation.service';
 import { ReservationController } from './reservation.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReservationEntity } from './entities/reservation.entity';
+import { TablesEntity } from 'src/tables/entities/table.entity';
+import { TablesModule } from 'src/tables/tables.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ReservationEntity])],
+  imports: [
+    TypeOrmModule.forFeature([ReservationEntity, TablesEntity]),
+    TablesModule,
+  ],
   controllers: [ReservationController],
   providers: [ReservationService],
 })
--- src/reservation/reservation.service.ts
+++ src/reservation/reservation.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Between, Repository } from 'typeorm';
 import { ReservationEntity } from './entities/reservation.entity';
 import { CreateReservationDTO } from './dto/create-reservation.dto';
 import { UpdateReservationDTO } from './dto/update-reservation.dto';
+import * as moment from 'moment';
 
 
 @Injectable()
@@ -12,16 +13,6 @@ export class ReservationService {
     @InjectRepository(ReservationEntity)
     private readonly reservationRepository: Repository<ReservationEntity>,
   ) {}
-
-  async create(createReservationDTO: CreateReservationDTO): Promise<ReservationEntity> {
-    const { userId, tableId, reservationDate } = createReservationDTO;
-    const reservation = this.reservationRepository.create({
-      user: { user_id: userId },
-      table: { table_id: tableId },
-      reservation_date: reservationDate,
-    });
-    return await this.reservationRepository.save(reservation);
-  }
 
   async getAllReservations() {
     return await this.reservationRepository
@@ -35,9 +26,43 @@ export class ReservationService {
         'user.user_id',
         'user.user_name',
         'table'
-        
       ])
       .getMany();
+  }
+
+  async create(createReservationDTO: CreateReservationDTO): Promise<ReservationEntity> {
+    const { userId, tableId, reservationDate } = createReservationDTO;
+
+    // Define a duração de cada slot de tempo em minutos
+    const slotDurationMinutes = 60; // 1 hora
+    const slotsPerHour = 60 / slotDurationMinutes;
+
+    // Obtem a data e hora inicial para a reserva
+    const startDateTime = moment(reservationDate).startOf('hour');
+
+    // Divide o período de reserva em slots de tempo e verifica cada um deles
+    for (let slot = 0; slot < slotsPerHour; slot++) {
+      const slotStartDateTime = startDateTime.clone().add(slot * slotDurationMinutes, 'minutes');
+      const slotEndDateTime = slotStartDateTime.clone().add(slotDurationMinutes, 'minutes');
+
+      const existingReservation = await this.reservationRepository.findOne({
+        where: {
+          table: { table_id: tableId },
+          reservation_date: Between(slotStartDateTime.toDate(), slotEndDateTime.toDate()),
+        },
+      });
+
+      if (existingReservation) {
+        throw new ConflictException('Esta mesa já está reservada para esse horário.');
+      }
+    }
+
+    const reservation = this.reservationRepository.create({
+      user: { user_id: userId },
+      table: { table_id: tableId },
+      reservation_date: reservationDate,
+    });
+    return await this.reservationRepository.save(reservation);
   }
 
   async findById(reservationId: string): Promise<ReservationEntity> {
